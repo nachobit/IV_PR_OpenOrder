e277aed21974db59597c46ab614ee0925d744890
nachobit <Nacho@MacBook-Pro-de-Ignacio.local>
2015-10-29 22:25:27 +0100

    update hito2

---
+++ openGestion/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='openGestion',
+	version='0.0.1',
+	description='Aplicación web CRM para el manejo de clientes',
+	url='https://github.com/nachobit/IV_PR_OpenOrder',
+	author='Ignacio Romero Cabrerizo',
+	author_email='nachorc@correo.ugr.es',
+	license='GNU General Public License',
+	packages=['openGestion'],
+	install_requires=[
+		'Flask',
+	],
+	zip_safe=False)
