e497aa338a3b97b95e13a2f4a7cffa8a80acfcb6
nachobit <Nacho@MacBook-Pro-de-Ignacio.local>
2015-10-30 15:28:18 +0100

    update hito2

--- openGestion/test_u.py
+++ openGestion/test_u.py
@@ -8,14 +8,13 @@ def fun(x):
 class TestCode(unittest.TestCase):
     def test_code(self):
         self.test_app = app.test_client()
-        #self.test_app = app.test_client()
         
         #Test demo
         self.assertEqual(fun(3), 4)
 
         #Test Response is 200 OK
         response = self.test_app.get('/gestion', follow_redirects=True)
-        self.assertEqual(response.status, " OK")
+        self.assertEqual(response.status, "200 OK")
 
         # Test logging out
         logout = self.test_app.get('/logout', follow_redirects=True)
