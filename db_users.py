6c31bd271d743d633747c93561d4882650ba507a
nachobit <Nacho@MacBook-Pro-de-Ignacio.local>
2015-11-27 15:44:18 +0100

    add db_users.py

---
+++ db_users.py
@@ -0,0 +1,10 @@
+from project import db
+from project.models import User
+
+# insert data
+db.session.add(User("pepito", "pepe@email.com", "1234"))
+db.session.add(User("admin", "ad@admin.com", "admin"))
+#db.session.add(User("mike", "mike@herman.com", "tell"))
+
+# commit the changes
+db.session.commit()
