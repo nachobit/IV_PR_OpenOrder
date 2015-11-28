5089c8950396f4a3757eb4fd8cd7690ea8b8aa5d
nachobit <Nacho@MacBook-Pro-de-Ignacio.local>
2015-11-28 18:48:23 +0100

    loginform

--- openGestion/users/forms.py
+++ openGestion/users/forms.py
@@ -2,9 +2,13 @@ from flask.ext.wtf import Form
 from wtforms import BooleanField, TextField, PasswordField, validators
 from wtforms.validators import Required, EqualTo, Email
 
+#class LoginForm(Form):
+#    username = TextField('Username', [validators.Length(min=4, max=25)])
+#    password = PasswordField('Password', [validators.Length(min=1, max=25)])
+
 class LoginForm(Form):
-    username = TextField('Username', [validators.Length(min=4, max=25)])
-    password = PasswordField('Password', [validators.Length(min=1, max=25)])
+    username = TextField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
 
 class RegisterForm(Form):
     username = TextField('username', [validators.Length(min=4, max=25)])
